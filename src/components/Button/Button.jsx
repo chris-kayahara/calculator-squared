301e4d341e8e1e932e0b51aaa61338474127e380
Chris Kayahara <kayahara.christopher@gmail.com>
2023-05-26 14:08:54 -0400

    Setup overall styling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.scss'
 
 export default function Button({ value, onClick }) {
     return (
-        <button className="button" onClick={onClick}>
+        <button className={(value === "+" | value === "-" | value === "x" | value=== "/") ? "button__operator" : "button"} onClick={onClick}>
             {value}
         </button>
     )
